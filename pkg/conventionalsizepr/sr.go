660706a9aea160c18cd507c417fc14ef9ced11c6
Mickael Stanislas <contact@mickael-stanislas.com>
2023-03-30 21:20:22 +0200

    fix: sizer labeler

--- pkg/conventionalsizepr/sr.go
+++ pkg/conventionalsizepr/sr.go
@@ -11,24 +11,29 @@ const (
 var (
 	Sizes = map[Size]size{
 		SizeXS: {
-			count: 10,
-			size:  "XS",
+			range_start: 0,
+			range_end:   49,
+			size:        "XS",
 		},
 		SizeS: {
-			count: 50,
-			size:  "S",
+			range_start: 50,
+			range_end:   99,
+			size:        "S",
 		},
 		SizeM: {
-			count: 100,
-			size:  "M",
+			range_start: 10,
+			range_end:   499,
+			size:        "M",
 		},
 		SizeL: {
-			count: 500,
-			size:  "L",
+			range_start: 500,
+			range_end:   999,
+			size:        "L",
 		},
 		SizeXL: {
-			count: 1000,
-			size:  "XL",
+			range_start: 1000,
+			range_end:   100000,
+			size:        "XL",
 		},
 	}
 )
@@ -36,13 +41,24 @@ var (
 type Size int
 
 type size struct {
-	count int
-	size  string
+	range_start int
+	range_end   int
+	size        string
 }
 
-// GetCount returns the count of the size
-func (s Size) GetCount() int {
-	return Sizes[s].count
+// GetRangeStart returns the range start of the size
+func (s Size) GetRangeStart() int {
+	return Sizes[s].range_start
+}
+
+// GetRangeEnd returns the range end of the size
+func (s Size) GetRangeEnd() int {
+	return Sizes[s].range_end
+}
+
+// IsInRange returns true if the value is in the range
+func (s Size) IsInRange(value int) bool {
+	return value >= s.GetRangeStart() && value <= s.GetRangeEnd()
 }
 
 // GetSize returns the size name.
@@ -74,14 +90,15 @@ func NewPRSize(addition, deletion int) *PrSize {
 // defineSize returns the size of the PR
 func (p *PrSize) defineSize() {
 
+	p.size = SizeXL
+
 	for _, x := range []Size{SizeXS, SizeS, SizeM, SizeL, SizeXL} {
-		if p.diff <= x.GetCount() {
+		if x.IsInRange(p.diff) {
 			p.size = x
-			return
+			break
 		}
 	}
 
-	p.size = SizeXL
 }
 
 // GetAddition returns the addition of the PR
